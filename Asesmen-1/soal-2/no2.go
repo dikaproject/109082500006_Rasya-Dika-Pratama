f86d8d3323875b64f6846108ea02b2a74fa5c36d
dikaproject <dikagilang2007@gmail.com>
2026-04-07 16:26:30 +0700

    upload: asesmen 1 alpro praktek

---
+++ Asesmen-1/soal-2/no2.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func tanggunganHari(jumlahHari int, tujuan string) int {
+    if tujuan == "domestik" {
+        if jumlahHari > 3 {
+            return 3
+        }
+        return jumlahHari
+    } else {
+        if jumlahHari > 8 {
+            return 8
+        }
+        return jumlahHari
+    }
+}
+
+func biayaPerHari(jumlahMhs int) int {
+    return 620000 * jumlahMhs
+}
+
+func perhitunganBiaya(jumlahMhs, lamaPerjalanan int, tujuan string, totalBiaya *float64) {
+    hari := tanggunganHari(lamaPerjalanan, tujuan)
+    harian := biayaPerHari(jumlahMhs)
+
+    if tujuan == "domestik" {
+        *totalBiaya = float64(hari) * float64(harian)
+    } else {
+        *totalBiaya = float64(hari) * float64(harian) * 1.5
+    }
+}
+
+func main() {
+    var jumlah, lama int
+    var tujuan string
+    var biaya float64
+
+    fmt.Print("masukkan jumlah mahasiswa : ")
+    fmt.Scan(&jumlah)
+    fmt.Print("Masukkan lama hari study tour : ")
+    fmt.Scan(&lama)
+    fmt.Print("Masukkan tujuan study tour (domestik/mancanegara) : ")
+    fmt.Scan(&tujuan)
+
+    perhitunganBiaya(jumlah, lama, tujuan, &biaya)
+
+    fmt.Printf("\nBiaya perjalanan yang harus dikeluarkan Tel-U : Rp. %.0f\n", biaya)
+}
